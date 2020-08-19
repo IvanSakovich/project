a2298e99b36801eb6a1208795d2c2760e4106f08
IvanSakovich <59743774+IvanSakovich@users.noreply.github.com>
2020-08-19 12:03:12 +0300

    Initial commit

---
+++ project/src/components/Content/Content.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './../../App.css';
+
+const Content = () =>{
+  return (
+    <div className="content">
+      <div>Content</div>
+      <img src='https://static3.depositphotos.com/1000454/256/i/450/depositphotos_2567474-stock-photo-wide-panorama-of-french-alps.jpg'></img>
+      <div>ava+desc</div>
+      <div>My Posts</div>
+      <div>New Post</div>
+      <div>Post</div>
+    </div>
+  );
+}
+
+export default Content;
---
+++ project/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './../../App.css';
+
+const Navbar = () =>{
+  return (
+    <div className="nav">
+      <div>Profile</div>
+      <div>Messages</div>
+      <div>News</div>
+      <div>Music</div>
+      <div>Settings</div>
+    </div>
+  );
+}
+
+export default Navbar;
