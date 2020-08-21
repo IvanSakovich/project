2adb1dcfcb46c104f04c75593b1102c584936eb3
IvanSakovich <59743774+IvanSakovich@users.noreply.github.com>
2020-08-21 11:04:30 +0300

    NavLink, Route 22

--- project/src/App.js
+++ project/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Content from './components/Content/Content';
+import Dialogs from './components/Dialogs/Dialogs';
+import Footer from './components/Footer/Footer';
+import {Route, BrowserRouter} from 'react-router-dom';
 
-const App = () =>{
+const App = (props) =>{
   return (
+    <BrowserRouter>
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Content /> 
+      <div className='app-content'>
+        <Route path='/content' component={Content}/>
+        <Route path='/dialogs' component={Dialogs}/>
+      </div>
+      <Footer />
     </div>
+    </BrowserRouter>
   );
 }
-
+ 
 export default App;
